5ae2edc0d5261201549b09cfc8c9db3f30634833
mauriciovancine <mauricio.vancine@gmail.com>
2019-11-05 12:05:10 -0300

    aulas 00-05 pdf

--- 01_aulas/convert_pdf.R
+++ 01_aulas/convert_pdf.R
@@ -11,9 +11,11 @@ setwd("/home/mude/data/github/minicurso-r-see-2019/01_aulas")
 dir()
 
 # convert rmarkdown
-purrr::map(dir(pattern = ".Rmd"), pagedown::chrome_print)
+pagedown::chrome_print("00_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
+pagedown::chrome_print("01_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
+pagedown::chrome_print("02_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
+pagedown::chrome_print("03_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
+pagedown::chrome_print("04_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
+pagedown::chrome_print("05_aula_minicurso_r_see_2019.Rmd", timeout = 1000)
 
-pagedown::chrome_print("00_aula_minicurso_r_see_2019.Rmd", 
-                       timeout = 1000)
-
-  # end ---------------------------------------------------------------------
+# end ---------------------------------------------------------------------
